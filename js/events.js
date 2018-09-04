16d7c2336d5b1b60e62cf6f604281273ac442280
Jess Guttenberg <jessigu@gmail.com>
2018-09-04 23:14:29 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,7 +19,11 @@ function pressIt() {
   })
 }
 
-function submitIt()
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+  })
+}
 
 $(document).ready(function(){
 
